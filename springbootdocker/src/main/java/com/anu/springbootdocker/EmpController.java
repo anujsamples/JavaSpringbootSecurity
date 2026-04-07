0d9719d059e99120ae206f14f4739849e1264166
anu <anujalaru@gmail.com>
2026-04-07 17:07:59 -0400

    basic project

---
+++ springbootdocker/src/main/java/com/anu/springbootdocker/EmpController.java
@@ -0,0 +1,14 @@
+package com.anu.springbootdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmpController {
+    @GetMapping("/hello")
+    public String getName(){
+        return "Hello this is spring";
+    }
+}
